6fb87d80886a0988c35bc86217e24ca6d6f5a85a
fucaobao <466368084@qq.com>
2015-12-07 23:22:59 +0800

    增加百度统计代码

---
+++ assets/script/analyze.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "//hm.baidu.com/hm.js?172ea9fb4a84dc7d35122364cec8ea63";
+  var s = document.getElementsByTagName("script")[0]; 
+  s.parentNode.insertBefore(hm, s);
+})();
