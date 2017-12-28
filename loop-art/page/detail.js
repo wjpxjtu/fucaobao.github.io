fb7a02481a1ad5e7cc8bc3679282eab516790ec5
fucaobao <466368084@qq.com>
2017-12-28 18:14:44 +0800

    修改页面展现方式(未完成)

--- loop-art/page/detail.js
+++ loop-art/page/detail.js
@@ -1,5 +1,25 @@
 ;
 $(function () {
+    function loadImages(sources, cb) {
+        var count = 0,
+            imgNum = sources.length;
+        for (var i = 0; i < sources.length; i++) {
+            var src = sources[i];
+            var image = new Image();
+            image.onload = function () {
+                if (++count >= imgNum) {
+                    typeof cb === 'function' && cb(sources);
+                }
+            };
+            image.src = src;
+        }
+    }
+    var projectMap = {
+        bchx: {
+            design: 10,
+            real: 9
+        }
+    };
     //获取URL中的值
     function getQuery(name, url) {
         var u = url || window.location.search,
@@ -8,20 +28,24 @@ $(function () {
         return r != null ? r[2] : '';
     }
     var p = getQuery('p'),
-        t = getQuery('t'),
-        n = getQuery('n'),
         f = '.jpg';
-    var path = '../images/projects/' + p + '/' + t + '/' + n + f;
-    loadImage(path, function(){
-        $('.content>.img-wrapper').removeClass('loading').find('img').attr('src', path);
+    var parent = '../images/projects/' + p + '/';
+    var images = [];
+    for (var i = 1; i <= projectMap[p].design; i++) {
+        images.push(parent + 'design/' + i + f);
+    }
+    for (var j = 1; j <= projectMap[p].real; j++) {
+        images.push(parent + 'real/' + j + f);
+    }
+    loadImages(images, function(list){
+        console.log(list);
     });
-
     $(document)
-    .on('click', '.more', function(){
+    .on('click', '.more', function () {
         $(this).toggleClass('active');
         $('.content .detail').toggleClass('dn');
     })
-    .on('click', '.second', function(){
+    .on('click', '.second', function () {
         window.location.href = MAP['projects'];
     });
 });
--- loop-art/page/projects.js
+++ loop-art/page/projects.js
@@ -1,14 +1,5 @@
 ;
 $(function () {
-    function getQuery(name, str) {
-        var u = str || window.location.search,
-            reg = new RegExp('(^|&)' + name + '=([^&]*)(&|$)'),
-            r = u.substr(u.indexOf('\?') + 1).match(reg);
-        return r != null ? r[2] : '';
-    }
-
-    var currentPage = getQuery('page', window.location.hash.substring(1)) || 1;//当前页面
-
     $(document)
     .on('click', '.body .nav li', function(){
         var $this = $(this);
@@ -17,64 +8,8 @@ $(function () {
     .on('click', '.demos li', function () {
         var $this = $(this);
         var p = $this.attr('data-p'),
-            t = $this.attr('data-t'),
-            n = $this.attr('data-n');
-        window.location.href = '../page/detail.html?p=' + p + '&t=' + t + '&n=' + n;
+            t = $this.attr('data-t');
+        window.location.href = '../page/detail.html?p=' + p + '&t=' + t;
     })
-    .on('click', '.page .left', function () {
-        var total = +$('.page .num').attr('data-total'),
-            num = +$('.page .num li.active').attr('data-num');
-        if (num > 1) {
-            changePage(num - 1);
-            $('.page .num li').eq(num - 2).addClass('active').siblings('li').removeClass('active');
-        }
-    })
-    .on('click', '.page .right', function () {
-        var total = +$('.page .num').attr('data-total'),
-            num = +$('.page .num li.active').attr('data-num');
-        if (num < total) {
-            changePage(num + 1);
-            $('.page .num li').eq(num).addClass('active').siblings('li').removeClass('active');
-        }
-    })
-    .on('click', '.page .num li', function () {
-        var dest = +$(this).attr('data-num'),
-            total = +$('.page .num').attr('data-total'),
-            num = +$('.page .num li.active').attr('data-num');
-        if (dest != num) {
-            changePage(dest);
-            $('.page .num li').eq(dest - 1).addClass('active').siblings('li').removeClass('active');
-        }
-    });
-
-    var $selector = $('.demos li'),
-        len = $selector.length,
-        maxPage = Math.ceil(len / 10);
-
-    function changePage(page) {
-        page = page || currentPage;
-        if (currentPage > maxPage) {
-            page = 1;
-        }
-        [].forEach.call($selector, function (item, index) {
-            if ((index >= 10 * (page - 1)) && (index < 10 * page)) {
-                $(item).removeClass('dn');
-            } else {
-                $(item).addClass('dn');
-            }
-        });
-        window.location.hash = '#page=' + page;
-    }
-    changePage();
-    var pageList = [];
-    for (var i = 0; i < maxPage; i++) {
-        if (currentPage > maxPage && i === 0) {
-            pageList.push('<li class="active" data-num="1"></li>');
-        } else if (i + 1 == currentPage) {
-            pageList.push('<li class="active" data-num="' + (i + 1) + '"></li>');
-        } else {
-            pageList.push('<li data-num="' + (i + 1) + '"></li>');
-        }
-    }
-    $('.page .num').attr('data-total', pageList.length).html(pageList.join(''));
+    ;
 });
