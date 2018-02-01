d0e362e61c45ccba83314774cfc381fd211a48ab
fucaobao <466368084@qq.com>
2018-02-01 18:39:29 +0800

    增加预加载功能

--- loop-art/page/news.js
+++ loop-art/page/news.js
@@ -7,6 +7,17 @@ $(function() {
         syhtw: 8
     };
 
+    function loadImage(src) {
+        var img = new Image();
+        img.src = src;
+    }
+    for (var key in MAP) {
+        for (var i = 0; i < MAP[key]; i++) {
+            var src = '../images/news/' + key + '/' + (i + 1) + '.jpg';
+            loadImage(src);
+        }
+    }
+
     $(document).on('click', '.more', function() {
         var $this = $(this);
         var type = $this.closest('li').attr('data-type');
