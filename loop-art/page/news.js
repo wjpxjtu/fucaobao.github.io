59063d57bc1c98271a098a2226a40adcf687eb2d
fucaobao <466368084@qq.com>
2018-02-01 18:33:51 +0800

    完善JS

--- loop-art/page/news.js
+++ loop-art/page/news.js
@@ -1,3 +1,17 @@
-;$(function () {
+;
+$(function() {
+    var MAP = {
+        bchx: 4,
+        gdbs: 2,
+        sygjsd: 6,
+        syhtw: 8
+    };
 
+    $(document).on('click', '.more', function() {
+        var $this = $(this);
+        var type = $this.closest('li').attr('data-type');
+        if (!MAP.hasOwnProperty(type)) {
+            return;
+        }
+    });
 });
